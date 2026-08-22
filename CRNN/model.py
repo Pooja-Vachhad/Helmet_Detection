3bacd06928fa906fc619bb791b0134896ad29a0b
Pooja Vachhad <likeway64@gmail.com>
2026-08-22 19:12:02 +0530

    Update model.py

--- CRNN/model.py
+++ CRNN/model.py
@@ -3,55 +3,48 @@ CRNN (Convolutional Recurrent Neural Network) Architecture
 Built from scratch for number plate recognition using CTC loss
 """
 
+from torchvision import models
 import torch.nn as nn
 
-# CNN + RNN model 
 class CRNN(nn.Module):
-    def __init__(self, H, W, num_classes):
-        super(CRNN, self).__init__()
+   def __init__(self , H , W , num_classes):
+     super().__init__()
+     self.H = H
+     self.W = W
+     self.num_classes = num_classes
 
-        self.cnn = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2, 2),
+     resnet = models.resnet18(pretrained = True)
+     self.cnn = nn.Sequential(*list(resnet.children())[:7])
 
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2, 2),
+     self.fc = nn.Sequential(
+         nn.Linear(1792, 128),
+         nn.ReLU(),
+         nn.Dropout(0.5)
+     )
 
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(kernel_size=(2, 1))
-        )
+     self.lstm  = nn.LSTM(
+         input_size = 128,
+         hidden_size = 128 ,
+         num_layers = 2,
+         batch_first = True ,
+         bidirectional = True ,
+         dropout = 0.3
+     )
 
-        self.fc = nn.Sequential(
-            nn.Linear(256 * (H // 8), 128),
-            nn.ReLU(),
-            nn.Dropout(0.5)
-        )
+     self.classifier = nn.Linear(128 * 2 , num_classes)
 
-        self.lstm = nn.LSTM(
-            input_size=128,
-            hidden_size=128,
-            num_layers=2,
-            batch_first=True,
-            bidirectional=True,
-            dropout=0.3
-        )
 
-        self.classifier = nn.Linear(128 * 2, num_classes)
+   def forward(self , x):
+      x = self.cnn(x)
+      x = x.permute(0 , 3 , 1 , 2)
+      B , T , C , H = x.size()
+      x = x .reshape(B , T , C * H)
+      x = self.fc(x)
+      x , _ = self.lstm(x)
+      x = self.classifier(x)
 
-    def forward(self, x):
-        x = self.cnn(x)
-        x = x.permute(0, 3, 1, 2) 
-        B, T, C, H = x.size()
-        x = x.reshape(B, T, C * H)
+      return  x
 
-        x = self.fc(x)
-        x, _ = self.lstm(x)
-        x = self.classifier(x)
 
-        return x
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = CRNN(H = 100 , W = 200 , num_classes  = num_classes).to(device)
